3860c90fac911d9bed2a812df42e8623528c6fb1
Jules Dejaeghere <dej.j@live.be>
2024-06-29 20:30:04 +0200

    bump version 1.0.0 -> 2.0.0

--- src/open_irceline/__init__.py
+++ src/open_irceline/__init__.py
@@ -2,4 +2,4 @@ from .api import IrcelineRioClient, IrcelineForecastClient, IrcelineApiError
 from .belaqi import belaqi_index_rio_hourly, belaqi_index_forecast_daily, belaqi_index_daily, belaqi_index_hourly
 from .data import RioFeature, ForecastFeature, FeatureValue, BelAqiIndex
 
-__version__ = '1.0.0'
+__version__ = '2.0.0'
