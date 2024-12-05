7d38b44eb631ad29a25116d86c8fcb83aee6a840
Jules Dejaeghere <dej.j@live.be>
2024-12-05 23:18:32 +0100

    bump version 3.0.1 -> 3.0.2

--- src/open_irceline/__init__.py
+++ src/open_irceline/__init__.py
@@ -3,4 +3,4 @@ from .data import RioFeature, ForecastFeature, FeatureValue, RioIfdmFeature
 from .forecast import IrcelineForecastClient
 from .rio import IrcelineRioClient, IrcelineRioIfdmClient
 
-__version__ = '3.0.1'
+__version__ = '3.0.2'
